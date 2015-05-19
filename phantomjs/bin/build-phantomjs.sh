6ceef1711f703010402b49d619efee7c4c07177b
Rui Carmo <rui.carmo@gmail.com>
2015-05-19 14:32:04 +0000

    Working build

--- phantomjs/bin/build-phantomjs.sh
+++ phantomjs/bin/build-phantomjs.sh
@@ -8,8 +8,6 @@ git config --global user.name "Docker User"; git clone https://github.com/ariya/
 # Check it out and perform the build
 cd /home/phantomjs; git checkout 2.0; ./build.sh --confirm; make clean
 # Remove all the packages we don't need inside the container
-apt-get remove -y build-essential g++ flex bison gperf ruby perl \
-  libsqlite3-dev libfontconfig1-dev libicu-dev libssl-dev \
-  libpng-dev libjpeg-dev python git; apt-get autoremove -y; apt-get clean
+apt-get remove -y build-essential g++ flex bison gperf ruby perl python git; apt-get autoremove -y; apt-get clean
 # Put the binary someplace obvious and remove the source tree
-mv /home/phantomjs /bin/phantomjs; rm -rf /home/phantomjs
+mv /home/phantomjs/bin/phantomjs /bin/phantomjs; rm -rf /home/phantomjs
