9776330e599b3b77982f19602a2a3b3e22504f59
Rui Carmo <rui.carmo@gmail.com>
2015-11-11 12:19:07 +0000

    pin version

--- phantomjs/bin/build-phantomjs.sh
+++ phantomjs/bin/build-phantomjs.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y build-essential g++ flex bison gperf ruby perl \
 # Clone repo
 git config --global user.name "Docker User"; git clone https://github.com/ariya/phantomjs.git /home/phantomjs
 # Check it out and perform the build
-cd /home/phantomjs; git checkout master; ./build.sh --confirm; make clean
+cd /home/phantomjs; git checkout 2.0; ./build.sh --confirm; make clean
 # Remove all the packages we don't need inside the container
 apt-get remove -y build-essential g++ flex bison gperf ruby perl python git; apt-get autoremove -y; apt-get clean
 # Put the binary someplace obvious and remove the source tree
