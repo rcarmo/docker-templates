0f6997a247de32e7dd169fc2da7cd55e028f556e
Rui Carmo <rui.carmo@gmail.com>
2016-10-19 08:48:24 +0000

    forgot extra resolutions

--- azure-sandbox/root/quickstart.sh
+++ azure-sandbox/root/quickstart.sh
@@ -5,7 +5,7 @@ echo "MPTcXfgXGiY=" | base64 -d > $PASSWD
 chown user:user $PASSWD
 chmod 600 $PASSWD
 # start VNC server without authentication
-su - user -c "vncserver -SecurityTypes None"
+su - user -c "vncserver -SecurityTypes None -depth 16 -geometry 1024x768 -geometry 1280x800 -geometry 1366x768 -geometry 1600x1200 -geometry 1280x720 -geometry 1920x1080 -geometry 2048x1536"
 echo "*WARNING* VNC Server launched without any authentication mechanism. Please consider using SSH tunneling instead."
 echo "You can now connect to localhost:5901 using a VNC client."
 echo "*NOTE* Terminating this shell will also kill your desktop."
